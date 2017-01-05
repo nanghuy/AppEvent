368575180d4e52e4a6cb05b53a2fdf437661365c
Kata Mr <ngdinhkhoa120691@gmail.com>
2017-01-05 15:13:58 +0700

    update SearchVC

--- AppEvent/SearchVC.swift
+++ AppEvent/SearchVC.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import MapKit
+
+private enum SuggestCell:String {
+    case Suggest = "SearchCell"
+    case SuggestCell = "searchCell"
+}
+
+private let numberOfItemPerRowPortrait : CGFloat = 3.0
+private let numberOfItemPerRowLandscape : CGFloat = 5.0
+private let leftAndRightPaddingsPortrait: CGFloat = 8.0 * ( numberOfItemPerRowPortrait + 1 )
+private let leftAndRightPaddingsLandscape: CGFloat = 8.0 * ( numberOfItemPerRowLandscape + 1 )
 
 class SearchVC: UIViewController {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    var searchController : UISearchController!
+    
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var clvSuggest: UICollectionView!
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
-        self.tabBarItem.title = ""
+        topConstraint.constant = (navigationController?.navigationBar.frame.size.height)!
+        bottomConstraint.constant = (tabBarController?.tabBar.frame.size.height)!
+        view.layoutIfNeeded()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        guard let flowLayout = clvSuggest.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+        
+        let width = ( CGRectGetWidth(clvSuggest.frame) - leftAndRightPaddingsLandscape ) / numberOfItemPerRowLandscape
+        flowLayout.itemSize = CGSizeMake(width, width)
+        
+        flowLayout.invalidateLayout()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        // add searchBar
+        self.searchController = UISearchController(searchResultsController:  nil)
+        
+        self.searchController.searchResultsUpdater = self
+        self.searchController.delegate = self
+        self.searchController.searchBar.delegate = self
+        
+        self.searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.dimsBackgroundDuringPresentation = true
+        
+        self.navigationItem.titleView = searchController.searchBar
+        
+        self.definesPresentationContext = true
+
+        // Regist cell
+        clvSuggest.registerNib(UINib.init(nibName: SuggestCell.Suggest.rawValue, bundle: nil), forCellWithReuseIdentifier: SuggestCell.SuggestCell.rawValue)
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +74,11 @@ class SearchVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+
+    }
 
     /*
     // MARK: - Navigation
@@ -39,3 +91,34 @@ class SearchVC: UIViewController {
     */
 
 }
+
+extension SearchVC : UICollectionViewDataSource {
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(SuggestCell.SuggestCell.rawValue, forIndexPath: indexPath) as! SearchCell
+        
+        return cell
+    }
+}
+
+extension SearchVC : UICollectionViewDelegate {
+    
+}
+
+extension SearchVC : UISearchControllerDelegate {
+    
+}
+
+extension SearchVC : UISearchBarDelegate {
+    
+}
+
+extension SearchVC : UISearchResultsUpdating {
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        
+    }
+}
+
+
