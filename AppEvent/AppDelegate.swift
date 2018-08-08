891254fde402e09be042d1a261c7a1af90352314
NguyenDinhKhoa <khoa.dinh@codecomplete.jp>
2018-08-08 16:34:17 +0700

    // Branch Test 1

--- AppEvent/AppDelegate.swift
+++ AppEvent/AppDelegate.swift
@@ -17,8 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        // Branch 1: commit 1
-        // Branch 1: commit 2
+        // Branch Test 1
         
         return true
     }
